d9d0c1e95399a7a1ae43b86cea0eb9f78bcb565c
Lemonade-up <wmaglangit1118@gmail.com>
2021-12-02 18:44:26 +0800

    Initial Commit, Pseudo Code added

---
+++ Program no.1.py
@@ -0,0 +1,16 @@
+# Program 1:
+# Create a program that ask for a sentence as input.
+# Display the number of words, vowels and consonants in the input 
+# ex.
+# input: Bahala kayo dyan
+# output:
+# words: 3
+# vowels: 6
+# consonants: 8
+
+# STEPS
+# 1 Input
+
+# 2 Loop
+# 3 Calculate
+# 4 Print
