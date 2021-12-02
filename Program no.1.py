4cfd741a2b2f8e464d80ddcaac55963e256d5b85
Lemonade-up <wmaglangit1118@gmail.com>
2021-12-02 21:36:54 +0800

    If-else statement for consonants added

--- Program no.1.py
+++ Program no.1.py
@@ -21,11 +21,13 @@ for l in range(len(sentence)):
     
     if sentence[l] == 'a' or sentence[l] == 'e'or sentence[l] == 'i' or sentence[l] == 'o' or sentence[l] == 'u':
         numVowels = numVowels + 1
+    elif sentence[l] == 'b' or sentence[l] == 'c' or sentence[l] == 'd' or sentence[l] == 'f' or sentence[l] == 'g' or sentence[l] == 'h' or sentence[l] == 'j' or sentence[l] == 'k' or sentence[l] == 'l' or sentence[l] == 'm' or sentence[l] == 'n' or sentence[l] == 'p' or sentence[l] == 'q' or sentence[l] == 'r' or sentence[l] == 's' or sentence[l] == 't' or sentence[l] == 'v' or sentence[l] == 'w' or sentence[l] == 'x' or sentence[l] == 'y' or sentence[l] == 'z':
+        numConsonants = numConsonants + 1
     elif sentence[l] == '1' or sentence[l] == '2' or sentence[l] == '3' or sentence[l] == '4' or sentence[l] == '5' or sentence[l] == '6' or sentence[l] == '7' or sentence[l] == '8' or sentence[l] == '9' or sentence[l] == '0':
         numNumbers = numNumbers + 1
     elif (sentence[l].isspace()) == True:
         numWords = numWords + 1
-
+    
 
 if sentence[-1].isspace() == True:
     numWords = numWords - 1
