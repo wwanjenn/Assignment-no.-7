85c0a4d14cd386b8646c86a3f7da03d747978959
Lemonade-up <wmaglangit1118@gmail.com>
2021-12-02 22:01:12 +0800

    Psuedo Code added

---
+++ Program no.2.py
@@ -0,0 +1,31 @@
+# Program 2: Password validator
+# Create a program that check if password is valid
+# The password is valid if all criteria are met:
+# a. Greater than 15 letters
+# b. Have at least one capital letter
+# c. Have at least one number
+# d. Have at least one special char (!@#$%^&*()_+ etc)
+# ex.
+# input: P@ssw0rd+P@ssw0rd
+# ouput: Valid
+
+# STEPS
+
+# 1 PRINT TITLE AND GUIDLINES
+
+# 2 INPUT
+
+# 3 LOOP
+
+# 4 15 LETTERS
+
+# 5 1 CAPITAL LETTER
+
+# 6 1 NUMBER
+
+# 7 1 SPECIAL CHAR
+
+# 8 CALCULATE
+
+# 9 OUTPUT
+
